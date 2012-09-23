89add63d665316c87b2ab4e63ab812eaf4a5cea9
Alex Ogier <alex.ogier@gmail.com>
2012-09-23 07:31:41 -0400

    Rip out all the cruddy stuff in exampleapp

--- exampleapp.py
+++ exampleapp.py
@@ -2,111 +2,20 @@
 
 from __future__ import unicode_literals
 
-import base64
 import os
-import os.path
-import urllib
-import hmac
 import json
-import hashlib
-from base64 import urlsafe_b64decode, urlsafe_b64encode
 
 import requests
+requests = requests.session()
+
 from flask import Flask, session, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-FB_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-requests = requests.session()
 
-app_url = 'https://graph.facebook.com/{0}'.format(FB_APP_ID)
-FB_APP_NAME = json.loads(requests.get(app_url).content).get('name')
+FB_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 FB_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-def oauth_login_url(preserve_path=True, next_url=None):
-    fb_login_uri = ("https://www.facebook.com/dialog/oauth"
-                    "?client_id=%s&redirect_uri=%s" %
-                    (app.config['FB_APP_ID'], request.url_root))
-
-    if app.config['FBAPI_SCOPE']:
-        fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
-    return fb_login_uri
-
-
-def simple_dict_serialisation(params):
-    return "&".join(map(lambda k: "%s=%s" % (k, params[k]), params.keys()))
-
-
-def base64_url_encode(data):
-    return base64.urlsafe_b64encode(data).rstrip('=')
-
-
-def fbapi_get_string(path,
-    domain=u'graph', params=None, access_token=None,
-    encode_func=urllib.urlencode):
-    """Make an API call"""
-
-    if not params:
-        params = {}
-    params[u'method'] = u'GET'
-    if access_token:
-        params[u'access_token'] = access_token
-
-    for k, v in params.iteritems():
-        if hasattr(v, 'encode'):
-            params[k] = v.encode('utf-8')
-
-    url = u'https://' + domain + u'.facebook.com' + path
-    params_encoded = encode_func(params)
-    url = url + params_encoded
-    print url
-    result = requests.get(url).content
-
-    return result
-
-
-def fbapi_auth(code):
-    params = {'client_id': app.config['FB_APP_ID'],
-              'redirect_uri': request.url_root + 'auth/',
-              'client_secret': app.config['FB_APP_SECRET'],
-              'code': code}
-
-    result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
-                              encode_func=simple_dict_serialisation)
-
-    pairs = result.split("&", 1)
-    result_dict = {}
-    for pair in pairs:
-        (key, value) = pair.split("=")
-        result_dict[key] = value
-    return (result_dict["access_token"], result_dict["expires"])
-
-
-def fbapi_get_application_access_token(id):
-    token = fbapi_get_string(
-        path=u"/oauth/access_token",
-        params=dict(grant_type=u'client_credentials', client_id=id,
-                    client_secret=app.config['FB_APP_SECRET']),
-        domain=u'graph')
-
-    token = token.split('=')[-1]
-    if not str(id) in token:
-        print 'Token mismatch: %s not in %s' % (id, token)
-    return token
-
-
-def fql(fql, token, args=None):
-    if not args:
-        args = {}
-
-    args["query"], args["format"], args["access_token"] = fql, "json", token
-
-    url = "https://api.facebook.com/method/fql.query"
-
-    r = requests.get(url, params=args)
-    return json.loads(r.content)
 
 
 def fb_call(call, args=None):
@@ -116,13 +25,11 @@ def fb_call(call, args=None):
     return json.loads(r.content)
 
 
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object('conf.Config')
 app.secret_key = SECRET_KEY
 db = SQLAlchemy(app)
-
 
 
 class Event(db.Model):
